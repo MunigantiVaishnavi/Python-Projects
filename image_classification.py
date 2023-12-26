c5d6cd7dc69e14049831a541f377475934ba5b38
MunigantiVaishnavi <153470706+MunigantiVaishnavi@users.noreply.github.com>
2023-12-26 11:06:31 +0530

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -1,14 +1,16 @@
+#importing dependencies
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 %matplotlib inline
-#usig pandas to read the database stored in the same folder
-data=pd.read_csv('mnist.csv')
-#viewing the column heads
+#using pandas to read the database stored in the same folder
+data=pd.read_csv('mnist_test.csv')
+data=pd.read_csv('mnist_train.csv')
+#viewing column heads
 data.head()
-#extracting data from the dataset and viewing them up close
+#extracting data from the dataset and viewing them up
 a=data.iloc[3,1:].values
 #reshaping the extracted data into a reasonable size
 a=a.reshape(28,28).astype('uint8')
@@ -17,9 +19,9 @@ plt.imshow(a)
 #separating labels and data values
 df_x=data.iloc[:,1:]
 df_y=data.iloc[:,0]
-#creating test and train sizes
-x_train, x_test, y_train, y_test= train_test_split(df_x,df_y,test_size=0.2, random_state=4)
-#check data
+#creating test and train sizes/batches
+x_train,x_test,y_train,y_test=train_test_split(df_x,df_y,test_size=0.2,random_state=42)
+#checking data
 y_train.head()
 #callig rf classifier
 rf=RandomForestClassifier(n_estimators=100)
@@ -38,5 +40,5 @@ for i in range(len(pred)):
 count
 #total values that the prediction code was running on
 len(pred)
-#acuuracy value
-pred/count
+#accuracy value
+11607/12000
