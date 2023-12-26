609da56a66fa8c73786c66cb33739b38ab7d76d5
MunigantiVaishnavi <153470706+MunigantiVaishnavi@users.noreply.github.com>
2023-12-26 16:43:35 +0530

    Create Dataset_classification.py

---
+++ Dataset_classification.py
@@ -0,0 +1,12 @@
+import sys
+print('Python: {}'.format(sys.version))
+import scipy
+print(scipy.__version__)
+import numpy as np
+print(np.__version__)
+import matplotlib
+print(matplotlib.__version__)
+import pandas
+print(pandas.__version__)
+import sklearn
+print(sklearn.__version__)
